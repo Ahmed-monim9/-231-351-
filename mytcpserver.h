300c61ddf6911618292ad046c03a1748002be48b
Ahmed-monim9 <165670889+Ahmed-monim9@users.noreply.github.com>
2024-10-18 22:56:27 +0300

    Update mytcpserver.h

--- mytcpserver.h
+++ mytcpserver.h
@@ -11,14 +11,15 @@ class MyTcpServer : public QObject
 {
     Q_OBJECT
 public:
-    explicit MyTcpServer(QObject *parent = nullptr);
+    explicit MyTcpServer(QObject *parent = nullptr); //Создает объект сервера QTcpServer.
+Настраивает сервер на прослушивание подключений от клиентов.
     ~MyTcpServer();
 public slots:
     void slotNewConnection();
-    void slotClientDisconnected();
-    void slotServerRead();
+    void slotClientDisconnected(); //Удаляет запись о клиенте из карты mClientSockets при его отключении.
+    void slotServerRead(); //Читает данные из клиентского сокета.
 private:
-    QTcpServer * mTcpServer;
+    QTcpServer * mTcpServer; //Указатель на объект сервера, который прослушивает подключения.
     std::map<int, QTcpSocket*> mClientSockets;
     //int server_status;
 };
