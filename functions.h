28f71b56689e1de75ec32cda04d70b4534b744c9
Ahmed-monim9 <165670889+Ahmed-monim9@users.noreply.github.com>
2024-10-19 10:20:32 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -2,10 +2,10 @@
 #define FUNCTIONS_H
 #include <QString>
 
-    QByteArray parsing (QString data_from_client);
+    QByteArray parsing (QString data_from_client);//Разбирает строку данных, полученную от клиента, и вызывает  функцию для обработки (работа с графом).
     QByteArray auth (QString log, QString pass);
-    QByteArray reg (QString log, QString pass, QString mail);
-    QByteArray graph (QStringList array);
+    QByteArray reg (QString log, QString pass, QString mail);//Обрабатывает запрос на авторизацию пользователя.
+    QByteArray graph (QStringList array);//связанные с операциями над графом, такими как поиск пути.
 
 
 #endif // FUNCTIONS_H
